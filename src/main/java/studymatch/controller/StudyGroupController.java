c39348ef6d7658f4043f804f8b8678e28c209d44
Joon <wjdwns011007@naver.com>
2025-11-17 13:11:53 +0900

    feat(controller): StudyGroupController 구현

--- src/main/java/studymatch/controller/StudyGroupController.java
+++ src/main/java/studymatch/controller/StudyGroupController.java
@@ -1,4 +1,27 @@
 package studymatch.controller;
 
+import studymatch.service.StudyGroupService;
+import studymatch.view.InputView;
+import studymatch.domain.person.Leader;
+import studymatch.domain.selection.SelectionResult;
+import studymatch.view.OutputView;
+
 public class StudyGroupController {
+    private final StudyGroupService service;
+    private final InputView input;
+    private final OutputView output;
+
+    public StudyGroupController(StudyGroupService service, InputView input, OutputView output) {
+        this.service = service;
+        this.input = input;
+        this.output = output;
+    }
+
+    public void run() {
+        Leader leader = input.inputLeader();
+        int count = input.askApplicantCount();
+        var pool = service.generateApplicants();
+        SelectionResult result = service.matchStudyGroup(leader, pool);
+        output.printResult(result);
+    }
 }
