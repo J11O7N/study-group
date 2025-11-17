871d396c373910545e026b77bb2817087806930a
Joon <wjdwns011007@naver.com>
2025-11-17 13:11:31 +0900

    feat(service): StudyGroupService 구현

--- src/main/java/studymatch/service/StudyGroupService.java
+++ src/main/java/studymatch/service/StudyGroupService.java
@@ -1,4 +1,20 @@
 package studymatch.service;
 
+import java.util.List;
+import studymatch.domain.person.Applicant;
+import studymatch.domain.person.Leader;
+import studymatch.support.generator.ApplicantGenerator;
+import studymatch.domain.selection.KeywordSelectionPolicy;
+import studymatch.domain.selection.SelectionResult;
+
 public class StudyGroupService {
+    private final ApplicantGenerator generator = new ApplicantGenerator();
+
+    public List<Applicant> generateApplicants() {
+        return generator.createApplicants(100); // 100명 생성
+    }
+
+    public SelectionResult matchStudyGroup(Leader leader, List<Applicant> applicants) {
+        return KeywordSelectionPolicy.select(leader, applicants);
+    }
 }
